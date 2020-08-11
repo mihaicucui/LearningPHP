30fe2fe8f4e78c946d758bc80880fb097417a570
Mihai Cucui <mihai.cucui@bitstone.eu>
2020-08-11 16:31:57 +0300

    12

--- index.php
+++ index.php
@@ -169,18 +169,35 @@ $array1 = array(array(77, 87), array(23, 45));
 $array2 = array("w3resource", "com");
 function merge_arrays_by_index($x, $y)
 {
-    $temp = array(); $temp[] = $x; if(is_scalar($y))
-{
-    $temp[] = $y;
-}
-else
-{
-    foreach($y as $k => $v)
-    {
-        $temp[] = $v;
+    $temp = array();
+    $temp[] = $x;
+    if (is_scalar($y)) {
+        $temp[] = $y;
+    } else {
+        foreach ($y as $k => $v) {
+            $temp[] = $v;
+        }
     }
-}
     return $temp;
 }
-echo '<pre>'; print_r(array_map('merge_arrays_by_index',$array2, $array1));
 
+echo '<pre>';
+print_r(array_map('merge_arrays_by_index', $array2, $array1));
+
+
+//12
+
+function upperOrLower($arr, $option){
+    if(strcmp(strtolower($option),'upper')==0)
+        foreach ($arr as $value){
+            print_r(strtoupper($value));
+        }
+    else{
+        foreach ($arr as $value){
+            print_r(strtolower($value));
+        }
+    }
+}
+
+$color = array('A' => 'Blue', 'B' => 'Green', 'c' => 'Red');
+upperOrLower($color, 'upper');
